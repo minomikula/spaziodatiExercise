f835687fce1c7a9a7f798cc6e1683740a616df8e
minomikula <mino.mikula@gmail.com>
2019-11-11 19:47:35 +0100

    toolbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { SecretComponent } from './secret/secret.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -28,7 +30,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ReactiveFormsModule,
     MatButtonModule,
     MatInputModule,
-    BrowserAnimationsModule
+    MatInputModule,
+    MatSelectModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
