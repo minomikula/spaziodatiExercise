daf37419d2055432fc835e5fd37c7b166ffc57f2
minomikula <mino.mikula@gmail.com>
2019-11-11 19:53:16 +0100

    move toolbar to app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  goToLogin() {
+    this.router.navigateByUrl('/login');
+  }
+
+  goToLogout() {
+    this.router.navigateByUrl('/logout');
+  }
+  goToSecret() {
+    this.router.navigateByUrl('/secret');
+  }
+
+  isLogged() {
+    return this.auth.isLogged();
+  }
+
+  isNotLogged() {
+    return this.auth.isLogged().pipe(
+      map(isLogged => !isLogged)
+    );
+  }
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../auth/auth.service';
-import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,31 +9,11 @@ export class HomeComponent implements OnInit {
 
 
   constructor(
-    private router: Router,
-    private auth: AuthService
+
   ) { }
 
   ngOnInit() {
   }
 
-  goToLogin() {
-    this.router.navigateByUrl('/login');
-  }
 
-  goToLogout() {
-    this.router.navigateByUrl('/logout');
-  }
-  goToSecret() {
-    this.router.navigateByUrl('/secret');
-  }
-
-  isLogged() {
-    return this.auth.isLogged();
-  }
-
-  isNotLogged() {
-    return this.auth.isLogged().pipe(
-      map(isLogged => !isLogged)
-    );
-  }
 }
